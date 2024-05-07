80e9dbf044edae295ee41d0839298975f3cf4287
linuxmaniaco <linuxmaniaco@gmail.com>
2024-05-07 19:44:33 -0300

    Rename class main

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,40 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        /**
+         * TODO
+         * Conhecer e importar classe scanner
+         * Exibir as mensagens para nosso usuário
+         * obter pelo scanner os valores digitados no terminal
+         * Exibir a mensagem "conta criada"
+         */
+
+        Scanner sc = new Scanner(System.in);
+
+        int agencia;
+        String conta;
+        String nome;
+        double saldo = 0;
+
+        System.out.println("Olá Como se chama? ");
+        nome = sc.nextLine();
+        System.out.println("Seja bem vindo, " + nome);
+
+        System.out.println("Me informe seus dados");
+        System.out.println("Qual número da sua agência:");
+        agencia = sc.nextInt();
+        sc.nextLine();
+        System.out.println("Agora o número da conta: ");
+        conta = sc.nextLine();
+        System.out.println("Quanto quer depositar: ");
+        saldo = sc.nextDouble();
+
+
+        System.out.println("Olá " +nome+", obrigado por criar uma conta em nosso banco, sua agência é: "+agencia+", conta: "+conta+" e seu saldo "+saldo+" já está disponivel para saque.");
+
+
+
     }
 }
